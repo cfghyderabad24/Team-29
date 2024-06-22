18e904277a7991dcb778d8e403929d89a9d39fc9
Debangshu_Saha <sahdebsaha909@gmail.com>
2024-06-22 23:36:26 +0530

    form work done

--- team29/src/main.jsx
+++ team29/src/main.jsx
@@ -5,6 +5,8 @@ import {Home,Donate,LoginType,Login,About} from './components/Services/index.js'
 import Layout from './Layout.jsx'
 import { createBrowserRouter,createRoutesFromElements,Route, RouterProvider } from 'react-router-dom'
 import NewForm from './components/ScholarshipForms/NewForm.jsx'
+import RegisteredStudent from './components/ScholarshipForms/RegisteredStudent.jsx'
+import Choice from './components/Choice/Choice.jsx'
 
 const router=createBrowserRouter(
   createRoutesFromElements(
@@ -13,8 +15,10 @@ const router=createBrowserRouter(
       <Route path="/logintype" element={<LoginType/>}/>
       <Route path="/login/:userType" element={<Login/>}/>
       <Route path="/registration" element={<NewForm/>}/>
+      <Route path="/registeredStudent" element={<RegisteredStudent/>}/>
       <Route path="/donate" element={<Donate/>}/>
       <Route path="/about" element={<About/>}/>
+      <Route path="/choice" element={<Choice/>}/>
     </Route>
   )
 )
