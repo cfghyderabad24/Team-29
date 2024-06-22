3e18781c63b755c0aa79dc22f47d2d0f6dd81e41
Debangshu_Saha <sahdebsaha909@gmail.com>
2024-06-22 18:36:36 +0530

    Basic setup and ready to work on donation

--- team29/src/main.jsx
+++ team29/src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {Home} from './components/Services/index.js'
+import {Home,Donate,LoginType,Login} from './components/Services/index.js'
 import Layout from './Layout.jsx'
-import LoginType from './components/Logintype/LoginType.jsx'
 import { createBrowserRouter,createRoutesFromElements,Route, RouterProvider } from 'react-router-dom'
-import Login from './components/login/Login.jsx'
 
 const router=createBrowserRouter(
   createRoutesFromElements(
@@ -13,6 +11,7 @@ const router=createBrowserRouter(
       <Route path="" element={<Home/>}/>
       <Route path="/logintype" element={<LoginType/>}/>
       <Route path="/login/:userType" element={<Login/>}/>
+      <Route path="/donate" element={<Donate/>}/>
       {/*<Route path="footer-contact" element={<FooterContact/>}/>
       <Route path="footer-KnowTheTeam" element={<FooterKnowTheTeam/>}/>
       <Route path="footer-reference" element={<FooterReference/>}/> */}
