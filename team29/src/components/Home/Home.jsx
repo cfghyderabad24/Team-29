ea74afa0f14c9bdb9b04a8bebe2bac8942b64da2
karthikeya21 <b.karthik2107@gmail.com>
2024-06-22 18:10:39 +0530

    login forms done

--- team29/src/components/Home/Home.jsx
+++ team29/src/components/Home/Home.jsx
@@ -1,9 +1,15 @@
-
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  
 
-export default Home
+  return (
+    <div className="home-container">
+      home
+    </div>
+  );
+};
+
+export default Home;
