2ccc304de55bf2740ac4564adf99d431917e2d78
Debangshu_Saha <sahdebsaha909@gmail.com>
2024-06-22 15:36:14 +0530

    Navbar setup

--- team29/src/components/Header/Header.jsx
+++ team29/src/components/Header/Header.jsx
@@ -2,7 +2,31 @@
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="navbar bg-base-100">
+        <div className="navbar-start">
+            <div className="dropdown">
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 font-Prata">
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Alumni</a></li>
+                <li><a>Donate</a></li>
+                <li><a>Contact</a></li>
+            </ul>
+            </div>
+        </div>
+        <div className="navbar-center hidden lg:flex font-Prata">
+            <ul className="menu menu-horizontal px-1">
+            <li><a>Home</a></li>
+            <li><a>About</a></li>
+            <li><a>Alumni</a></li>
+            <li><a>Donate</a></li>
+            <li><a>Contact</a></li>
+            </ul>
+        </div>
+        <div className="navbar-end">
+            <a className="btn font-Prata">Login</a>
+        </div>
+    </div>
   )
 }
 
