dae1fba6d8bd83e24e52fa44039fe1c95e60ed28
Debangshu_Saha <sahdebsaha909@gmail.com>
2024-06-23 05:48:57 +0530

    "changes to routes"

--- team29/Backend/src/routes/user.routes.js
+++ team29/Backend/src/routes/user.routes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-<<<<<<< HEAD
-import {registerUser,getUsers} from "../controllers/user.controllers.js";
-import {registeredUser, getRegisteredUsers} from "../controllers/registerUser.controller.js"
-=======
-import {registerUser,getUsers,deleteUser} from "../controllers/user.controllers.js";
 
->>>>>>> origin/main
+import {registerUser,getUsers,deleteUser} from "../controllers/user.controllers.js";
+import {registeredUser, getRegisteredUsers} from "../controllers/registerUser.controller.js"
+
+
+
 const router=Router()
 
 router.route("/register").post(registerUser)
@@ -14,11 +13,8 @@ router.route("/registered").post(registeredUser)
 // Add this route in user.routes.js
 
 router.route("/").get(getUsers);
-<<<<<<< HEAD
 router.route("/").get(getRegisteredUsers);
 
-=======
 router.route("/:id").delete(deleteUser);
->>>>>>> origin/main
 
 export default router
